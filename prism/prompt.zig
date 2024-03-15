8c4310e90512615d55b748ba1bdfa8a732b0bed5
pseudoc <atlas.yu@canonical.com>
2024-03-16 00:19:27 +0800

    refactor(prompt): terminal and its reader

--- prism/prompt.zig
+++ prism/prompt.zig
@@ -1,1 +1,20 @@
+const std = @import("std");
+const prism = @import("prism");
+
+pub const terminal = struct {
+    const stdout = std.io.getStdOut();
+
+    var maybe_instance: ?prism.Terminal = null;
+    pub var reader = prism.Terminal.EventReader{ .file = stdout };
+    pub var reader_mutex = std.Thread.Mutex{};
+
+    pub fn get() !prism.Terminal {
+        if (maybe_instance) |instance| {
+            return instance;
+        }
+        maybe_instance = try prism.Terminal.init(stdout);
+        return maybe_instance.?;
+    }
+};
+
 pub const input = @import("prompt/input.zig");
