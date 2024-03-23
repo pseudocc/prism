50f6dfc81bad26e6f0fbf7663d1b089286a05d33
pseudoc <atlas.yu@canonical.com>
2024-03-23 23:47:13 +0800

    feat(common): Optional(T)

--- prism/common.zig
+++ prism/common.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const int = u16;
 
 pub const Size = struct {
@@ -9,3 +10,68 @@ pub const Point = struct {
     x: int = 0,
     y: int = 0,
 };
+
+pub fn Optional(comptime StructType: type) type {
+    const T = std.builtin.Type;
+    const t = switch (@typeInfo(StructType)) {
+        .Struct => |case| case,
+        else => @compileError("Optional only works with structs"),
+    };
+
+    const proto = comptime this: {
+        var fields: [t.fields.len]T.StructField = undefined;
+        var i: usize = 0;
+
+        for (t.fields) |field| {
+            fields[i] = field;
+            if (@typeInfo(field.type) != .Optional) {
+                const ot = @Type(.{
+                    .Optional = .{ .child = field.type },
+                });
+                const ot_null: ot = null;
+                fields[i].type = ot;
+                fields[i].default_value = &ot_null;
+                fields[i].alignment = @sizeOf(ot);
+            }
+            i += 1;
+        }
+        var ot = t;
+        ot.fields = &fields;
+        ot.layout = .Auto;
+        ot.decls = &.{};
+
+        break :this ot;
+    };
+
+    return @Type(.{ .Struct = proto });
+}
+
+test "prism.common.Optional" {
+    const equals = std.testing.expectEqual;
+
+    const T1 = struct {
+        a: u8,
+        b: u8,
+    };
+    const OT1 = Optional(T1);
+    const ot1 = OT1{};
+    try equals(@as(?u8, null), ot1.a);
+    try equals(@as(?u8, null), ot1.b);
+
+    const T2 = struct {
+        const Self = @This();
+        a: ?u8 = 0,
+        b: u8,
+
+        pub fn sum(self: Self) u8 {
+            const a = self.a orelse 0;
+            return a + self.b;
+        }
+    };
+    const OT2 = Optional(T2);
+    std.log.debug("{}", .{OT2});
+
+    const ot2 = OT2{ .b = null };
+    try equals(@as(?u8, 0), ot2.a);
+    try equals(@as(?u8, null), ot2.b);
+}
